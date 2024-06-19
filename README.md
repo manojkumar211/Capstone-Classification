c7d764f34a67152684c655f1f5bbbf5113775758
manojkumar211 <mk.chinthakindi@gmail.com>
2024-06-19 11:25:50 +0530

    first

---
+++ README.md
@@ -0,0 +1,7 @@
+# About Dataset:
+
+```
+- Dataset is related to Arrhythmia disease which is nothing but blood circulation issue. The dataset is belongs to medical domain.
+- Dataset has 175729 - rows and 34 - columns.
+
+```
---
+++ src/project/data.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+# Load the data:
+
+df=pd.read_csv("C:/AI&ML Engineer/Projects/Apple/Apple Arrhythmia Prediction/Arrhythmia.csv")
+df.head()
+df.shape
